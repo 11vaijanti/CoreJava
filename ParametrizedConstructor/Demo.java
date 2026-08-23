90044f70cacd8ce3c89b69a0b95a6bcd4fadc1e8
Vaijanti Rajure <146207340+11vaijanti@users.noreply.github.com>
2026-08-23 23:22:19 +0530

    Add files via upload

---
+++ ParametrizedConstructor/Demo.java
@@ -0,0 +1,14 @@
+package ParametrizedConstructor;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Student s1 = new Student("raju",10);
+		System.out.println(s1.name);
+		System.out.println(s1.roll);
+		
+		
+		
+	}
+
+}
---
+++ ParametrizedConstructor/Student.java
@@ -0,0 +1,11 @@
+package ParametrizedConstructor;
+
+public class Student {
+	int roll ;
+	String name ;
+	
+	public Student(String n, int r){
+		name = n;
+		roll = r;
+	}
+}
